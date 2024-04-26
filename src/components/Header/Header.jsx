f82fd3793b25cdec5e0a70312df62d73c12b02a5
PiyushGaurav <piyushgaurav07@gmail.com>
2024-04-27 00:41:47 +0530

    added responsive navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import logo from '../../assets/logo.png';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<div className={styles.header_container}>
 			<div className={styles.logo_container}>
@@ -18,6 +21,20 @@ export default function Header() {
 					<button className={styles.signup}>Sign up</button>
 				</ul>
 			</nav>
+			<div className={styles.toggle_button} onClick={() => setIsOpen(isOpen => !isOpen)}>
+				<GiHamburgerMenu size={30} color="black" />
+			</div>
+			{isOpen && (
+				<div className={styles.mobile_navbar}>
+					<ul>
+						<li>Catalogue</li>
+						<li>Fashion</li>
+						<li>Favorite</li>
+						<li>Lifestyle</li>
+						<button className={styles.signup}>Sign up</button>
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
